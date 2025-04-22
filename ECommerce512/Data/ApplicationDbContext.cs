b7b0c301a6127635631dd015c5d77684107dd400
mohamed mokhlef <mohamedmokhlef@hotmail.com>
2025-04-22 18:52:01 +0200

    Configure DB

---
+++ ECommerce512/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce512.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce512.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public DbSet<Category> categories { get; set; }
+        public  DbSet<Brand> brands { get; set; }
+        public DbSet<Product> products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+           
+            optionsBuilder.UseSqlServer(connectionString: "Data Source =. ; Initial Catalog=ECommerce512; Integrated Security = True; TrustServerCertificate = True");
+        }
+
+    }
+}
