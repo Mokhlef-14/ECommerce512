ad5fb4ce00efd5a74121ed8813418ed7282c76e3
mohamed mokhlef <mohamedmokhlef@hotmail.com>
2025-05-10 19:01:30 +0300

    1- Add Same category products section 2-Add filter 3-Add pagination

--- ECommerce512/Controllers/HomeController.cs
+++ ECommerce512/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using System.Diagnostics;
+using System.Linq;
 using ECommerce512.Data;
 using ECommerce512.Models;
 using ECommerce512.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace ECommerce512.Controllers
 {
@@ -19,10 +21,44 @@ namespace ECommerce512.Controllers
 
 
 
-        public IActionResult Index()    
+        public IActionResult Index(int categortId,string? query,double MinPrice,double Maxprice,int page = 1)    
         {
-            var products = _context.products.Include(e => e.Category);
+            IQueryable<Product> products = _context.products.Include(e => e.Category);
+            var categories = _context.categories;
+            ViewData["categories"] = categories.ToList();
+
+            if (categortId > 0 && categortId < categories.Count())
+            {
+                products = products.Where(e => e.CategoryId == categortId);
+                ViewBag.categoryId = categortId;
+            }
+
+            if(query is not null)
+            {
+                products = products.Where(e => e.Name.Contains(query));
+                ViewBag.query = query;
+            }
+
+            if (MinPrice > 0)
+            {
+                products = products.Where(e => e.Price >= (decimal)MinPrice);
+                ViewBag.MinPrice = MinPrice;
+            }
+            if (Maxprice > 0)
+            {
+                products = products.Where(e => e.Price <= (decimal)Maxprice);
+                ViewBag.MaxPrice = Maxprice;
+            }
+
+            products = products.Skip((page-1) * 8).Take(8);
+            ViewBag.TotalProducts = Math.Ceiling(_context.sproducts.Count() / 8.0);
+           
+            
+            
+            
+
             return View(products.ToList());
+            
         }
 
 
@@ -36,11 +72,16 @@ namespace ECommerce512.Controllers
             if (product != null)
             {
                 var RelatedProducts = _context.products.Where(e => e.Name.Contains(product.Name.Substring(0,5))).Skip(0).Take(4).ToList();
+                var sameCategoryProducts = _context.products.Where(e => e.Category.Name == product.Category.Name && e.Id != product.Id).OrderBy(e => Guid.NewGuid())
+    .Take(4)
+    .ToList();
+
 
                 var ReturnedData = new ProductWithRelatedVM()
                 {
                     Product = product,
-                    RelatedProducts = RelatedProducts
+                    RelatedProducts = RelatedProducts,
+                    SameCategoryProducts = sameCategoryProducts
                 };
                 return View(ReturnedData);
             }
--- ECommerce512/Models/ViewModels/ProductWithRelatedVM.cs
+++ ECommerce512/Models/ViewModels/ProductWithRelatedVM.cs
@@ -3,7 +3,7 @@
     public class ProductWithRelatedVM
     {
         public Product Product { get; set; } = null!;
-
         public List<Product> RelatedProducts { get; set; } = null!;
+        public List<Product> SameCategoryProducts { get; set; } = null!;
     }
 }
